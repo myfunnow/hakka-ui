62c87e5f48689a69c507391fd71082020b15d5d3
gary.chu <garychu@myfunnow.com>
2023-03-07 16:59:41 +0800

    Switch library between v2 and v3 versions
    - Update library version from 0.0.0 to 0.0.1
    - Add aliases for `vue-demi` and `@vue/composition-api`
    - Add postinstall script to switch versions
    - Add CLI script to switch versions
    - Add `alias` for `@` in `vite.config.js`
    - Add `build:3` and `build:2` npm scripts
    - Add `pub` script for public
    
    [bundle-config/ui-library.config.js]
    - Add `path` import
    - Add `vue-demi` and `@vue/composition-api` aliases
    - Change entry point to `resolve('../src/index.js')`
    - Change output directory to `getOutputDir(version)`
    - Add `vue-demi` to external dependencies
    - Change `vue` global to `Vue`
    - Change `vue-demi` global to `V
    [scripts/postinstall.mjs]
    - Add postinstall script to switch versions
    - Import the version from 'vue-demi'
    - Import the switchVersion function from 'utils.mjs'
    [scripts/switch-cli.mjs]
    - Add a new file to switch the version of the CLI
    - Import the switchVersion function from `utils.mjs`
    - Read the version to switch to from the command line argument
    [vite.config.js]
    - Add `import path from 'path'`
    - Add `resolve` function
    - Add `alias` for `@`
    - Add `optimizeDeps` with `vue-demi` excluded
    [package.json]
    - Update version number from `0.0.0` to `0.0.1`
    - Move `src` folder to `scripts` folder
    - Add `switch:2` and `switch:3` npm scripts
    - Add `build:3` and `build:2` npm scripts
    - Replace `build` script with a combination of `build:3` and `build:2`
    - Add `pub` script for public npm publishing
    - Add
    [scripts/utils.mjs]
    - Add functions to switch between v2 and v3 versions of the library
    - Add functions to copy directories recursively
    - Add function to get the output directory for a given version

--- bundle-config/ui-library.config.js
+++ bundle-config/ui-library.config.js
@@ -1,27 +1,52 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { isVue2, version } from 'vue-demi'
+import vue2Compiler from 'vue-template-compiler'
+import * as vue3Compiler from '@vue/compiler-sfc'
+import vue2 from '@vitejs/plugin-vue2'
+import vue3 from '@vitejs/plugin-vue'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
+import { getOutputDir } from '../scripts/utils.mjs'
+
+const vuePlugin = isVue2 ? vue2 : vue3
+const compiler = isVue2 ? vue2Compiler : vue3Compiler
+const resolve = (str) => path.resolve(__dirname, str)
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('../src'),
+      vue: isVue2 ? resolve('./node_modules/vue2') : resolve('./node_modules/vue3'),
+      '@vue/composition-api': resolve('./node_modules/@vue/composition-api')
+    }
+  },
   build: {
     lib: {
-      entry: "src/index.js",
+      entry: resolve('../src/index.js'),
       name: 'Zoek-Design',
       formats: ["es", "cjs", "umd"],
       fileName: format => `zoek-design.${format}.js`,
       assetFileNames: (assetInfo) => assetInfo.name,
     },
     rollupOptions: {
+      external: ['vue-demi', 'vue'],
       output: {
+        dir: getOutputDir(version),
         globals: {
-          vue: 'Vue',
+          'vue': 'Vue',
+          'vue-demi': 'VueDemi',
         },
       },
     },
+    optimizeDeps: {
+      exclude: ['vue-demi']
+    },
   },
   plugins: [
-    vue({
-      compileTemplate: true,
+    vuePlugin({
+      compiler,
+      compileTemplate: false,
       template: { optimizeSSR: true },
       data: { // This helps to inject variables in each <style> tag of every Vue SFC
         scss: () => `@import "@/assets/styles/main.scss";`,
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import vuetify from 'vite-plugin-vuetify'
 
+const resolve = (str) => path.resolve(__dirname, str)
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+    }
+  },
   plugins: [vue(), vuetify(), WindiCSS()],
+  optimizeDeps: {
+    exclude: ['vue-demi']
+  },
 })
