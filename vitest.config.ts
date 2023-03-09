84a9ee6f34e7835be3949073ffd74d4214a5a430
gary.chu <garychu@myfunnow.com>
2023-03-09 17:23:14 +0800

    Update ZdPagination component tests
    - Add a ResizeObserver mock for the tests of the ZdPagination component
    - Add tests for the ZdPagination component, including a snapshot test, a test to check the page action button text, and a test to check the displayed text
    - Add an alias for the `@` symbol to the `resolve` field in `vite.config.js` and `vitest.config.ts`
    - Add the `vue`
    
    [src/components/zd-pagination/__snapshots__/zd-pagination.test.js.snap]
    - Add snapshot for zd-pagination component
    [vite.config.js]
    - Add an `alias` to the `resolve` object in `vite.config.js` to map `@` to `./src`
    - Import `fileURLToPath` and `URL` from `node:url` in `vite.config.js`
    [package.json]
    - Add `@vue/test-utils` and `jsdom` packages
    - Update `@vitest/ui` package to version `0.28.5`
    [src/components/zd-pagination/zd-pagination.test.js]
    - Add a ResizeObserver mock for the tests
    - Add tests for the ZdPagination component
    - Add a snapshot test
    - Add a test to check the page action button text
    - Add a test to check the displayed text
    [vitest.config.ts]
    - Add an alias for the `@` symbol to the `resolve` field
    - Add the `vue` and `vuetify` plugins
    - Add a `test` field with globals, environment, include and deps configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from 'vite'
-
 import vue from '@vitejs/plugin-vue'
-import WindiCSS from 'vite-plugin-windicss'
 import vuetify from 'vite-plugin-vuetify'
 
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [vue(), vuetify(), WindiCSS()],
+  plugins: [
+    vue(),
+    vuetify({
+      autoImport: true,
+    })
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    deps: {
+      inline: ['vuetify'],
+    },
+  },
 })
