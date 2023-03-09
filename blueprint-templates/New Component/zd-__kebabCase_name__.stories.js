37b77931911e8e6720a485bc7cd14ea55ea3ccc8
gary.chu <garychu@myfunnow.com>
2023-03-09 10:19:13 +0800

    Update Blueprint Templates and ZdPagination Component
    - Change the `blueprint-templates/New` template from `$props` to `args` and remove the `computed` block
    - Rename the `src/components/zd-pagination/zd-pagination.stories.js` component from `Pagination` to `ZdPagination` and change the template to use `args` instead of `$props`
    - Add a `count` prop to
    
    [blueprint-templates/New]
    - Remove the `computed` block
    - Change the template from `$props` to `args`
    [src/components/zd-pagination/zd-pagination.stories.js]
    - Rename component from `Pagination` to `ZdPagination`
    - Change the template to use `args` instead of `$props`
    - Add a `count` prop to the template with a value of `12`

--- blueprint-templates/New Component/zd-__kebabCase_name__.stories.js
+++ blueprint-templates/New Component/zd-__kebabCase_name__.stories.js
@@ -7,22 +7,9 @@ export default {
 
 export const Template = (args, { argTypes }) => ({
   components: { Zd{{pascalCase name}} },
-  computed: {
-    props() {
-      const finalProps = { ...this.$props }
-
-      // Reapply mappings to circumvent Storybook Vue 2 bug.
-      Object.entries(this.$props)
-        .filter(([key, value]) =>
-          Object.hasOwn(argTypes[key]?.mapping || {}, value),
-        )
-        .forEach(([key, value]) => {
-          finalProps[key] = argTypes[key].mapping[value]
-        })
-
-      return finalProps
-    },
+  setup() {
+    return { args };
   },
   props: Object.keys(argTypes),
-  template: '<zd-{{kebabCase name}} v-bind="$props"/>',
+  template: '<zd-{{kebabCase name}} v-bind="args"/>',
 });
--- src/components/zd-pagination/zd-pagination.stories.js
+++ src/components/zd-pagination/zd-pagination.stories.js
@@ -1,32 +1,14 @@
-import Pagination from './zd-pagination.vue';
+import ZdPagination from './zd-pagination.vue';
 
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Pagination',
-  component: Pagination,
+  component: ZdPagination,
 };
 
 export const Template = (args, { argTypes }) => ({
-  components: { Pagination },
-  computed: {
-    props() {
-      const finalProps = { ...this.$props }
-
-      // Reapply mappings to circumvent Storybook Vue 2 bug.
-      Object.entries(this.$props)
-        .filter(([key, value]) =>
-          Object.hasOwn(argTypes[key]?.mapping || {}, value),
-        )
-        .forEach(([key, value]) => {
-          finalProps[key] = argTypes[key].mapping[value]
-        })
-
-      return finalProps
-    },
+  components: { ZdPagination },
+  setup() {
+    return { args };
   },
   props: Object.keys(argTypes),
-  template: '<Pagination v-bind="$props"/>',
-});
+  template: '<zd-pagination v-bind="args" :count="12"/>',
+})
