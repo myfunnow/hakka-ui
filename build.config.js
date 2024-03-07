4c7467a03cba9fe702977be1ad9117c7661edb8d
gary.chu <garychu@myfunnow.com>
2024-03-07 04:11:38 +0100

    chore: refine typescript build configs

--- build.config.js
+++ build.config.js
@@ -2,14 +2,32 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    { builder: 'mkdist', input: './src/', outDir: './dist/' },
+    {
+      builder: 'mkdist',
+      input: './src',
+      pattern: ['**/*.vue'],
+      loaders: ['vue'],
+    },
+    {
+      builder: 'mkdist',
+      input: './src',
+      pattern: ['**/!(*.test|*.stories).(ts|js)'],
+      format: 'esm',
+      loaders: ['js'],
+    },
+    { builder: 'mkdist', input: './src/', pattern: ['**/*.scss'] },
+    {
+      builder: 'mkdist',
+      input: './src/assets/images/',
+      outDir: './dist/assets/images/',
+    },
   ],
   declaration: false,
   clean: true,
-  hooks: {
-    "mkdist:entry:options": (ctx, entry, mkdistOptions) => {
-      mkdistOptions.pattern = ['components/**/*.vue', '**/index.js', '**/*.scss']
-      return mkdistOptions
-    }
-  }
+  failOnWarn: false,
+  rollup: {
+    esbuild: {
+      minify: true,
+    },
+  },
 })
